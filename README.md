b9ae2e06e7a167c7c7b832ce882d8cb3158a7c79
JaesungHuh <JaesungHuh@github.com>
2023-10-16 17:28:37 +0100

    fix readme and model.py

--- README.md
+++ README.md
@@ -2,32 +2,63 @@
 
 TRAP allows for the robust prediction of CD8+ T-cell epitopes from MHC-I ligands. It employs a deep learning-based platform for predicting immunogenicity and a DecisionTree classifier to estimate the degree of correctness.
 
+This GitHub repository includes documentation for:
+* TRAP-CNN model structure (model.py) 
+* RSAT (RSAT.R) 
+
+## Installation
+``` 
+git clone https://github.com/ChloeHJ/TRAP.git
+```
+
+We highly recommend you to create conda environment with python version >= 3.9.
+
+```
+conda create -n trap python=3.9
+conda activate trap
+pip install -r requirements.txt
+```
+
+## Dash web application
 The dash web application can be found here: http://35.176.114.129:8050/ 
 
+## Data
 All required data, models and supplementary notebooks are stored in: https://drive.google.com/drive/folders/15A2P5xP2c-q48vVGPRB7h7uHEMycPYoX?usp=drive_link 
 
-We provide gbm_example_test_data.csv as an example test data. The input data should have the same structure as the example dataset, and contain peptides having 9-10 amino acids in length. Minimal requirments for the input data are Peptide sequence and -log2(NetMHCpan rank score). Please select the pathogenic or self-antigen (cancer, autoantigens, etc.) model and insert your list of peptides on the application. The model may take 5-10 minutes to generate the output. We highly recommend the users to have <100 test peptides at a time due to limited server space. The "Tutorial for TRAP web application" document contains a detailed tutorial on how to use the web application.
+We provide [gbm_example_test_data.csv](gbm_example_test_data.csv) as an example test data. The input data should have the same structure as the example dataset, and contain peptides having 9-10 amino acids in length. Minimal requirments for the input data are Peptide sequence and -log2(NetMHCpan rank score). Please select the pathogenic or self-antigen (cancer, autoantigens, etc.) model and insert your list of peptides on the application. The model may take 5-10 minutes to generate the output. We highly recommend the users to have <100 test peptides at a time due to limited server space. The "Tutorial for TRAP web application" document contains a detailed tutorial on how to use the web application.
 
 Interpretation of the result: 
 * TRAP: Immunogenicity (TRAP > 0.5: Positive, TRAP < 0.5: Negative)
 * MCDropout: MaxProb value from averaged 100 Monte Carlo dropouts 
 * Confidence: Degree of correctness predicted using MCDropout (TRAP > ~0.75: High confidence) 
 
-This GitHub repository includes documentation for:
-* TRAP-CNN model structure (model.py) 
-* RSAT (RSAT.R) 
-
 The data/ includes training data for these modules:
-* pathogenic_db.csv and selfantigen_data_forMODEL.csv are the training datasets for TRAP 
-* ood_dropout_pathogenic.csv and ood_dropout_selfantigen.csv are training data for OOD MCDropout-based OOD classifier
-* Autoantigen_5data.csv are autoantigen and cancer-associated antigens gathered from 5 databases. hsProtemeDF.RData is reference human proteome sequence for RSAT calculation. Autoantigen_peptide_vector.RData and iedb_dissimilar_proteome_blosum62.RData are processed files from RSAT calculations. 
+* pathogenic_db.csv and selfantigen_data_forMODEL.csv are the training datasets for TRAP. 
+* ood_dropout_pathogenic.csv and ood_dropout_selfantigen.csv are training data for OOD MCDropout-based OOD classifier.
+* Autoantigen_5data.csv are autoantigen and cancer-associated antigens gathered from 5 databases. 
+* hsProtemeDF.RData is reference human proteome sequence for RSAT calculation. 
+* Autoantigen_peptide_vector.RData and iedb_dissimilar_proteome_blosum62.RData are processed files from RSAT calculations. 
 
+## Citation
 Please cite TRAP as following: Lee, C. H., Huh, J., Buckley, P. R., Jang, M., Pinho, M. P., Fernandes, R. A., Antanaviciute, A., Simmons, A., & Koohy, H. (2023). A robust deep learning workflow to predict CD8 + T-cell epitopes. Genome Medicine, 15(1), 70. https://doi.org/10.1186/s13073-023-01225-z
 
 Available from: https://genomemedicine.biomedcentral.com/articles/10.1186/s13073-023-01225-z
 
+You could also use the BibTeX form below.
+```
+@article{lee2023robust,
+  title={A robust deep learning workflow to predict CD8+ T-cell epitopes},
+  author={Lee, Chloe H and Huh, Jaesung and Buckley, Paul R and Jang, Myeongjun and Pinho, Mariana Pereira and Fernandes, Ricardo A and Antanaviciute, Agne and Simmons, Alison and Koohy, Hashem},
+  journal={Genome Medicine},
+  volume={15},
+  number={1},
+  pages={70},
+  year={2023},
+  publisher={Springer}
+}
+```
 
-
+## License
 Shield: [![CC BY-NC-SA 4.0][cc-by-nc-sa-shield]][cc-by-nc-sa]
 
 This work is licensed under a
--- model.py
+++ model.py
@@ -1,46 +1,29 @@
+import re
+from typing import Any
+
 import numpy as np
 import pandas as pd
-import tensorflow as tf
-import sys
-import csv
-import random
-import os as os
-from io import StringIO
+
 import keras
-from tensorflow.keras import layers
-from keras.layers import Input,Dense,concatenate,Dropout,AveragePooling2D
-from keras.models import Model,load_model                                                      
-from keras import backend as K
+from keras.layers import Dense, concatenate, Dropout, Conv1D, GlobalMaxPooling1D
+from keras.models import Model                                                  
+
+from tensorflow.keras.utils import pad_sequences
 from tensorflow.keras.optimizers import Adam
-import matplotlib.pyplot as plt
-from sklearn.metrics import auc,precision_recall_curve,roc_curve,confusion_matrix
-from keras.models import Sequential, Model
-from keras.layers import Dense, Dropout, Activation, Flatten, Embedding, Input, LSTM, RNN, SimpleRNN, Bidirectional, Concatenate, GlobalMaxPool1D, Conv1D, MaxPooling1D, GlobalMaxPooling1D, Conv2D
-from keras.utils.vis_utils import plot_model
+
+from sklearn.metrics import auc, precision_recall_curve, roc_curve
+
 import transformers
-from transformers import T5Tokenizer, T5Model, BertModel, BertForMaskedLM, BertTokenizer, pipeline, BertConfig, EncoderDecoderConfig, EncoderDecoderModel
-from transformers import TFBertModel, TFXLNetModel, XLNetTokenizer,XLNetConfig, TFT5EncoderModel
-import re
-import pickle
-import seaborn as sb
-import matplotlib.pyplot as plt
-from keras.preprocessing.sequence import pad_sequences
-
-print('transformers version: %s' %transformers.__version__)
-print('tensorflow version: %s' %tf.__version__)
-print('keras version: %s' %keras.__version__)
+from transformers import T5Tokenizer, TFT5EncoderModel
 
 
-# plotting scripts 
-from tensorflow.keras.optimizers import Adam
-
-def evaluate_roc_pr_mlp_10cv(test_model, X_train, X_train_mlp, y_train):
-    # let's do a train/validation split
+def evaluate_roc_pr_mlp_10cv(test_model: Any, X_train: Any, X_train_mlp: Any, y_train: Any) -> None:
+    # let"s do a train/validation split
     bucket_roc = []
     bucket_pr = []
-    for i in range(10):
+    for _ in range(10):
         array = np.arange(len(X_train))
-        train_index = np.random.choice(array,int(len(X_train)*0.9),replace=False)
+        train_index = np.random.choice(array, int(len(X_train) * 0.9), replace=False)
         valid_index = [item for item in array if item not in train_index]
 
         input1_train = X_train[train_index]
@@ -51,8 +34,8 @@ def evaluate_roc_pr_mlp_10cv(test_model, X_train, X_train_mlp, y_train):
         label_valid = y_train[valid_index]
 
         model = test_model(X_train)
-        callback_val = keras.callbacks.EarlyStopping(monitor='val_loss', patience=15,restore_best_weights=False)
-        callback_train = keras.callbacks.EarlyStopping(monitor='loss',patience=2,restore_best_weights=False)
+        callback_val = keras.callbacks.EarlyStopping(monitor="val_loss", patience=15,restore_best_weights=False)
+        callback_train = keras.callbacks.EarlyStopping(monitor="loss", patience=2,restore_best_weights=False)
         history = model.fit(
             x=[input1_train, input1_mlp_train],   # feed a list into
             y=label_train,
@@ -64,96 +47,100 @@ def evaluate_roc_pr_mlp_10cv(test_model, X_train, X_train_mlp, y_train):
         y_true = label_valid
         y_pred = model.predict([input1_valid, input1_mlp_valid])
 
+        # You could use these to plot the roc curve
         fpr,tpr,_ = roc_curve(y_true,y_pred)
         area = auc(fpr,tpr)
         bucket_roc.append((fpr,tpr,_,area))
 
+        # You could use these to plot the precision-recall curve
         precision, recall, _ = precision_recall_curve(y_true, y_pred)
         area = auc(recall, precision)
         bucket_pr.append((precision, recall, _, area))
 
-    draw_roc(bucket_roc)
-    draw_pr(bucket_pr)
 
-
-def add_space_to_pep(peptides):
+def add_space_to_pep(peptides: Any) -> Any:
+    # Adding space to peptides
     peptide_space = [] 
     for ele in peptides:
         temp = [[]]
         for char in ele:
             temp.append([])
             temp[-1].append(char) 
-        peptide_space.append(' '.join(''.join(ele) for ele in temp))
+        peptide_space.append(" ".join("".join(ele) for ele in temp))
     peptide_space = [re.sub(r"[UZOB]", "X", sequence.lstrip()) for sequence in peptide_space]
 
     return peptide_space
 
 
-# load and process data 
-sdata = pd.read_csv('pathogenic_db.csv')
-sdata['Immunogenicity'] = sdata['Immunogenicity'].replace(['Positive', 'Positive-Low', 'Positive-Intermediate', 'Positive-High', 'Negative'], [1, 1,1,1,0])
-y_train  = sdata['Immunogenicity'].values
-peptides = sdata.ContactPosition.values
-input_peptides = add_space_to_pep(peptides)
-
-# load embedding model 
-tokenizer = T5Tokenizer.from_pretrained("Rostlab/prot_t5_xl_uniref50", do_lower_case=False )
-model = TFT5EncoderModel.from_pretrained("Rostlab/prot_t5_xl_uniref50", from_pt=True)
-
-# embed peptides 
-tokenized_texts = [tokenizer.tokenize(sent) for sent in input_peptides]
-input_ids = pad_sequences([tokenizer.convert_tokens_to_ids(txt) for txt in tokenized_texts], padding="pre")
-
-attention_masks = []
-for seq in input_ids:
-  seq_mask = [float(i>0) for i in seq]
-  attention_masks.append(seq_mask)
-
-embedding = model(input_ids=input_ids)[0]
-embedding = np.asarray(embedding)
-pickle.dump( embedding, open( "protT5_xl_peptides.pkl", "wb" ) )
-
-# load embedded sequence 
-protT5_xl_peptides_1 = pickle.load( open( "protT5_xl_peptides.pkl", "rb" ) )
-print(protT5_xl_peptides_1.shape)
-
-# cnn + hydrophobicity + mhc binding model 
-X_train_mlp = sdata[['hydrophobicity', 'nlog2Rank']]
-bert_embed_matrix = protT5_xl_peptides_1 
-def cnn1d_1_MH(bert_embed_matrix):
-    
+def cnn1d_1_MH(bert_embed_matrix: Any) -> Model:
+    # Model architecture
     s = keras.Input(shape=(bert_embed_matrix.shape[1],  bert_embed_matrix.shape[2]))
     emb = bert_embed_matrix.shape[2]
 
     mlp_input = keras.Input(shape=(2,))
 
-    pep_conv1 = Conv1D(emb, 1, padding='same', activation='relu', kernel_initializer='glorot_normal', name = 'kernel_1')(s)
+    pep_conv1 = Conv1D(emb, 1, padding="same", activation="relu", kernel_initializer="glorot_normal", name = "kernel_1")(s)
     pep_pool1 = GlobalMaxPooling1D()(pep_conv1)
-    pep_conv3 = Conv1D(emb, 3, padding='same', activation='relu', kernel_initializer='glorot_normal', name = 'kernel_3')(s)
+    pep_conv3 = Conv1D(emb, 3, padding="same", activation="relu", kernel_initializer="glorot_normal", name = "kernel_3")(s)
     pep_pool3 = GlobalMaxPooling1D()(pep_conv3)
-    pep_conv5 = Conv1D(emb, 5, padding='same', activation='relu', kernel_initializer='glorot_normal', name = 'kernel_5')(s)
+    pep_conv5 = Conv1D(emb, 5, padding="same", activation="relu", kernel_initializer="glorot_normal", name = "kernel_5")(s)
     pep_pool5 = GlobalMaxPooling1D()(pep_conv5)
-    pep_conv7 = Conv1D(emb, 7, padding='same', activation='relu', kernel_initializer='glorot_normal', name = 'kernel_7')(s)
+    pep_conv7 = Conv1D(emb, 7, padding="same", activation="relu", kernel_initializer="glorot_normal", name = "kernel_7")(s)
     pep_pool7 = GlobalMaxPooling1D()(pep_conv7)
     pep_cat = concatenate([pep_pool1, pep_pool3, pep_pool5, pep_pool7])
 
-    mlp = Dense(2000, activation='relu')(mlp_input)
+    mlp = Dense(2000, activation="relu")(mlp_input)
     merge = concatenate([pep_cat, mlp])
 
-    dense = Dense(256, activation='relu')(merge) 
+    dense = Dense(256, activation="relu")(merge) 
     dense = Dropout(0.1)(dense)
-    out = Dense(1, activation='sigmoid')(dense)
+    out = Dense(1, activation="sigmoid")(dense)
     model = Model(inputs=[s, mlp_input], outputs=[out])
 
-    model.compile(loss='binary_crossentropy',
+    model.compile(loss="binary_crossentropy",
                   optimizer= Adam(learning_rate = 0.00001, decay = 1e-6 ),
-                  metrics='accuracy')
-    
+                  metrics="accuracy")
     return model
 
-model = cnn1d_1_MH(bert_embed_matrix)
-model.summary()
+
+def main() -> None:
+    # load and process data 
+    sdata = pd.read_csv("data/pathogenic_db.csv")
+    sdata["Immunogenicity"] = sdata["Immunogenicity"].replace(["Positive", "Positive-Low", "Positive-Intermediate", "Positive-High", "Negative"], [1, 1,1,1,0])
+    y_train  = sdata["Immunogenicity"].values
+    peptides = sdata.ContactPosition.values
+    input_peptides = add_space_to_pep(peptides)
+
+    # load embedding model 
+    tokenizer = T5Tokenizer.from_pretrained("Rostlab/prot_t5_xl_uniref50", do_lower_case=False )
+    model = TFT5EncoderModel.from_pretrained("Rostlab/prot_t5_xl_uniref50", from_pt=True)
+
+    # embed peptides 
+    tokenized_texts = [tokenizer.tokenize(sent) for sent in input_peptides]
+    input_ids = pad_sequences([tokenizer.convert_tokens_to_ids(txt) for txt in tokenized_texts], padding="pre")
+
+    attention_masks = []
+    for seq in input_ids:
+        seq_mask = [float(i>0) for i in seq]
+        attention_masks.append(seq_mask)
+
+    embedding = model(input_ids=input_ids)[0]
+    embedding = np.asarray(embedding)
+    print("Embedding shape : ", embedding.shape)
+    np.save("protT5_xl_peptides.npy", embedding)
+
+    # cnn + hydrophobicity + mhc binding model 
+    X_train_mlp = sdata[["hydrophobicity", "nlog2Rank"]]
+    bert_embed_matrix = embedding 
+
+    model = cnn1d_1_MH(bert_embed_matrix)
+    model.summary()
+
+    X_train = embedding
+    evaluate_roc_pr_mlp_10cv(cnn1d_1_MH, X_train, X_train_mlp, y_train)
 
 
-X_train = protT5_xl_peptides_1
-evaluate_roc_pr_mlp_10cv(cnn1d_1_MH, X_train, X_train_mlp, y_train)
+if __name__ == "__main__":
+    print(f"transformers version: {transformers.__version__}")
+    print(f"keras version: {keras.__version__}")
+    main()
